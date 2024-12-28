6351093df02d27d7acecc7f8c19f06370572b275
Abhay Asodariya <sahildholariya070@gmail.com>
2024-12-28 20:49:59 +0530

    .

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
           </p>
 
           <div className="buttons flex flex-col lg:flex-row items-start lg:items-center gap-6 mt-5">
-            <div> 
+            <div>
               <a href="/SAHIL_RESUME.pdf" download="../../resume/SAHIL_RESUME.pdf" className="bg-gradient-to-r from-blue-500 to-purple-700 py-3 px-10 hover:from-purple-500 hover:to-blue-800 transition-all duration-200 ease-in-out  font-semibold text-white rounded-lg">GET  CV</a>
             </div>
             <Link to="" className="bg-gray-300 hover:bg-gradient-to-r hover:from-blue-500 hover:to-purple-700 hover:text-white transition-all duration-200 ease-in-out py-3 px-10  font-semibold  rounded-lg">Explore More</Link>
@@ -39,7 +39,7 @@ const About = () => {
         </div>
 
         <div className="image max-md:hidden max-md:mt-5 w-[70%] md:w-[60%] lg:w-[40%]">
-          <img src="../../images/sahil.webp" className=" rounded-xl shadow-xl hover:scale-110 cursor-pointer transition-all duration-200 ease-in-out" alt="" />
+          <img src="../assets/images/sahil.webp" className=" rounded-xl shadow-xl hover:scale-110 cursor-pointer transition-all duration-200 ease-in-out" alt="" />
         </div>
       </div>
 
@@ -58,7 +58,7 @@ const About = () => {
 
 
         <div className="second w-[80%] lg:w-[33%]">
-          <img src="../../images/mission-vision.webp" alt="" />
+          <img src="../assets/images/mission-vision.webp" alt="" />
         </div>
 
         <div className="third-content lg:w-[33%] flex flex-col gap-5">
